e00bc626353823b330333bab47b2f2e7a43a328f
Jan Clase <janclase02@gmail.com>
2026-06-20 14:32:31 -0400

    Update Vite base path for client configuration

--- Project/client/vite.config.js
+++ Project/client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/KyuuList",
+  base: process.env.VITE_BASE_PATH || "/KyuuList/client",
   server: {
     host: true,
     watch: {
